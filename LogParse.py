a19f26e0e045394d5d1cdd18d49a71764020611d
EC2 Default User <ec2-user@ip-172-31-72-201.ec2.internal>
2022-05-12 22:54:43 +0000

    LogParse2 - Regex for Cloud-init log

--- LogParse.py
+++ LogParse.py
@@ -15,7 +15,7 @@ def Parser(LogPath, Match):
 def ipChecker(InputIP):
     count = 0
     for i in InputIP.split('.'):
-        if (int(i)<255) and (int(i)>=0) and ((int(i[0]) == 0) and (int(i) == 0)):
+        if (int(i)<255) and (int(i)>=0) and ((int(i[0]) != 0) and (int(i) == 0)):
             count = count + 1
         else:
             print("invalid Ipv4")
@@ -24,5 +24,25 @@ def ipChecker(InputIP):
         print("Valid Ipv4")
     else:
         print("invalid Ipv4")
-#Parser('/home/ec2-user/environment/Python_Scripts/PythonScripts/testdir/testlog2', '35.172.155.192')   
-ipChecker('192.168.0.225')
+
+def LogParse2(LogPath):
+    '''Find runtime for each process in the log'''
+    logData = {}
+    
+    with open(LogPath, 'r') as f:
+        while f.readline():
+            line = f.readline()
+            #print(line)
+            match = re.search("[a-zA-Z_]+\.\w+", line)
+            if match != None:
+                #print(match.group(0))
+                filename = match.group(0)
+                logData[line[7:14]] = {"FileName":filename}
+    with open('/home/ec2-user/environment/Python_Scripts/PythonScripts/testdir/cloud-init-files', 'w') as f:
+        f.write(str(logData))
+        f.close()
+    
+        
+LogParse2('/home/ec2-user/environment/Python_Scripts/PythonScripts/testdir/cloud-init.log')   
+#ipChecker('192.168.0.225')
+
